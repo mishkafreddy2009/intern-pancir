e4d1d43d87523e3d0b6b0baef5443aeccd1939ea
Timofey Basuev <echiauta@gmail.com>
2024-07-02 12:27:19 +0500

    solve data types problems

--- data_types/1.py
+++ data_types/1.py
@@ -1,3 +1,13 @@
-base = [0, "hello", 1.23, "world", 3, 4, 7]
-nums = [n for n in base if isinstance(n, (int, float))]
-strs = [s for s in base if isinstance(s, str)]
+def main() -> int:
+    base = [0, "hello", 1.23, "world", 3, 4, 7]
+
+    nums = [n for n in base if isinstance(n, (int, float))]
+    strs = [s for s in base if isinstance(s, str)]
+
+    print(nums)
+    print(strs)
+
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
